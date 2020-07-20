a7d44fb6cbe8874016f5db40c7a0813d4823c9b1
thomaslnx <marcos.msilva10@gmail.com>
2020-07-19 21:15:24 -0300

    First skeleton for fake data to vanhack challenge

---
+++ server.js
@@ -0,0 +1,11 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+
+console.log(`Server is On on port: ${port}`);
+server.listen(port);
